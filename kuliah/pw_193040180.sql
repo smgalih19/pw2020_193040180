710577c8bb9b06c01785413878359534dd92a248
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2020-05-17 20:33:56 +0700

    update database terbaru

---
+++ kuliah/pw_193040180.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2020 at 03:32 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pw_193040180`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nrp` char(9) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(100) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(1, 'Sandhika Galih', '043040023', 'sandhikagalih@unpas.ac.id', 'Teknik Informatika', 'sandhika.jpg'),
+(2, 'Sugih Mohammad Galih', '193040180', '193040180@mail.unpas.ac.id', 'Teknik Informatika', 'sugih.jpg'),
+(3, 'Mohammad Sugih', '193391121', 'mohsugih@gmail.com', 'Teknik Mesin', 'mohsugih.jpeg'),
+(4, 'Rifki Perdiansyah', '193040188', 'rifky@mail.unpas.ac.id', 'Teknik Industri', 'rifky.jpeg'),
+(5, 'Cahyadi Ivansyah', '193040123', 'ivan@mail.unpas.ac.id', 'Teknik Mesin', 'ivan.jpeg'),
+(6, 'Claudio', '193040179', 'claudio@mail.unpas.ac.id', 'Teknik Lingkungan', 'claudio.jpeg'),
+(7, 'Dio Geovani', '193040124', 'dio@mail.unpas.ac.id', 'Perencanaan Wilayah dan Kota', 'dio.jpeg'),
+(8, 'Hermawan Arby', '193040161', 'hermawan@mail.unpas.ac.id', 'Teknik Informatika', 'arby.jpeg'),
+(9, 'Aldi Ageng', '193040054', 'aldi@mail.unpas.ac.id', 'Teknik Informatika', 'aldi.jpeg'),
+(10, 'Alip F', '193040158', 'alip@mail.unpas.ac.id', 'Teknik Informatika', 'alip.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(3, 'pw', '$2y$10$XB/3IP6lFJLrdh/HFIH9fOxrI3ZBgIExbiQyBrVuqRjTnwmAGYX.G'),
+(4, 'admin', '$2y$10$l0ngVTydZqc.E5Krad5BSetY.WHhK.nygvE7mxETaTZC6/mif111u'),
+(5, 'sugih', '$2y$10$0Oq0Tmxjzi5Pd58UjBPM3.HOpeJ1JNR35TBbHKoAxUtoIDVCJI7ri');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tubes/tubes_193040180.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2020 at 03:32 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tubes_193040180`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apparel`
+--
+
+CREATE TABLE IF NOT EXISTS `apparel` (
+  `id_apparel` int(11) NOT NULL,
+  `display` varchar(100) NOT NULL,
+  `jenis_apparel` varchar(100) NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `ukuran` varchar(100) NOT NULL,
+  `harga` int(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `apparel`
+--
+
+INSERT INTO `apparel` (`id_apparel`, `display`, `jenis_apparel`, `merk`, `ukuran`, `harga`) VALUES
+(1, 'display1.jpg', 'Kaos Pria', 'Deus Ex Machina', 'M, L', 250000),
+(2, 'display2.jpg', 'Hoodie Parachute', 'Sch', 'S, M, L, XL', 350000),
+(3, 'display3.jpg', 'Hoodie', 'Bloods', 'M, L, XL', 250000),
+(4, 'display4.jpg', 'Celana Jogger', 'DC', 'M, L, XL, XXL', 100000),
+(5, 'display5.jpg', 'Kaos Unisex', 'Green Light', 'M, L', 150000),
+(6, 'display6.jpg', 'Kemeja', 'Alisan', 'M, L, XL', 220000),
+(7, 'display7.jpg', 'Sporhem', '3 Second', 'M, L', 115000),
+(8, 'display8.jpg', 'Jas Formal', 'Zara', 'S, M, L, XL, XXL', 1500000),
+(9, 'display9.jpg', 'Kaos', '3 Second', 'S, M, L', 125000),
+(10, 'display10.jpg', 'Sporhem', 'Cardinal', 'S, M, L, XL', 250000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(2, 'sugih', '$2y$10$CPX/YvE/sBE6LJz7QzcGReLwQ068D0/vnutG1FhRZhorXN0nEOkOC'),
+(3, 'galih', '$2y$10$FHF0Y6kmd0U3d8gmq574oe7kmxiAWVCGnKb6Qa2o1w3W.TNmyG8lS'),
+(4, 'admin', '$2y$10$rQPA8PIy9H6dQJCdpSVmCetvk2yk2sq5AAFsbYbwjsQl3yLHLilem');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `apparel`
+--
+ALTER TABLE `apparel`
+  ADD PRIMARY KEY (`id_apparel`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apparel`
+--
+ALTER TABLE `apparel`
+  MODIFY `id_apparel` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
